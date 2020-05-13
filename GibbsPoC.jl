53991f1fc33624c47eaa6899fcc6725c9ba422a7
JacobRaymond <jacob.raymond97@gmail.com>
2020-05-13 16:03:42 -0400

    Added RQMC Method

--- GibbsPoC.jl
+++ GibbsPoC.jl
@@ -9,6 +9,9 @@ a=1.802
 del=1
 gam=0.1
 
+
+#### Crude Method (see DOI: 10.2307/2289776) ####
+
 #Initial values
 init=s./t #MLE
 b=(sum(init)+del)/(gam+10*a-1)#Mean of a IG(gamma+10a, ∑lambda+∂)
@@ -17,13 +20,11 @@ b=(sum(init)+del)/(gam+10*a-1)#Mean of a IG(gamma+10a, ∑lambda+∂)
 lambda_gibbs=[ones(10)]
 b_gibbs=Float16[]
 
-#### Crude Method (see DOI: 10.2307/2289776) ####
-
 #Gibbs Sampler
 
-n=40 #Number of times the sampler is repeated
+n=300 #Number of times the sampler is repeated
 for k in 1:n
-    m=200 #Number of iterations of the sampler
+    m=1021 #Number of iterations of the sampler
     for j in 1:m
         global b
         global lambda
@@ -44,6 +45,52 @@ for k in 1:n
     push!(lambda_gibbs, lambda)
     push!(b_gibbs, b)
 end
+#Calcualte the results
+deleteat!(lambda_gibbs, 1) #The first observation of lambda_gibbs is an array of 1s due to storage issues
+result=mean(lambda_gibbs)
+push!(result, mean(b_gibbs))
+println("[λ_1, ..., λ_10, β]=", round.(result, digits=3))
+
+#### RQMC Method (see  DOI: 10.1073/pnas.0409596102)####
+
+#Import Korobov sampler
+include("Korobov.jl")
+
+#Initial values
+init=s./t #MLE
+b=(sum(init)+del)/(gam+10*a-1)#Mean of a IG(gamma+10a, ∑lambda+∂)
+
+#Array to house the observations
+lambda_gibbs=[ones(10)]
+b_gibbs=Float16[]
+
+#Draw the lambdas
+m=200 #The algorithm is repeated 300 times, as per Owen and Tribble
+for k in 1:m
+    #Generate RMQC points
+    rqmc=Korobov(1021, 65, 11)
+
+    for j in 1:1021
+        global b
+        global lambda
+        global s
+        global t
+
+        #Draw the lambdas
+        lambda=Float16[quantile(Gamma(a+s[1], 1/(t[1]+1/b)), rqmc[j][1])]
+        for i in 2:10
+            push!(lambda, quantile(Gamma(a+s[i], 1/(t[i]+1/b)), rqmc[j][i]))
+        end
+
+        #Draw the beta
+        b=quantile(InverseGamma(gam+10*a, sum(lambda)+del), rqmc[j][11])
+    end
+
+    #Save the values
+    push!(lambda_gibbs, lambda)
+    push!(b_gibbs, b)
+end
+
 #Calcualte the results
 deleteat!(lambda_gibbs, 1) #The first observation of lambda_gibbs is an array of 1s due to storage issues
 result=mean(lambda_gibbs)
