6081e5ce4c8db8af8f836282657caa992d44b7b0
JacobRaymond <jacob.raymond97@gmail.com>
2020-06-09 23:02:44 -0400

    Add correct point order, everything works now

--- LCG.jl
+++ LCG.jl
@@ -1,32 +1,41 @@
 using Random
 
-function lcg(N, a, d, seed)
+function lcg(N, a, d)
 
 
     #Generate starting value
-    u=[seed]
+    u=[1/N]
     x=N*u
 
     #Generate the x and u
-    for i in 1:N
+    for i in 1:N-2
         x_i=(a*x[i])%N
         push!(x, x_i)
         push!(u, x_i/N)
     end
 
-    #Remove first observations
-    popfirst!(x)
-    popfirst!(u)
-
     #Initial point of the sequence
     z=[zeros(d)]
 
-    #Create points z
-    for j in 0:N-1
-        ind=collect((d*j+1):(d*j+d))
-        ind=(ind.-1).%N .+1
-        push!(z, u[ind])
-     end
+    #Generate the order of the first indexes for each point
+    seq=[1:1:N-1;]
+    indices=[1]
+    for j in 1:N-2
+        ind=(indices[j]+d)%(N-1)
+        if ind==0 || ind in indices
+            ind=minimum(setdiff(seq, indices))
+        end
+        push!(indices, ind)
+    end
+
+    #Order points
+    indices=map(x->[x:1:x+d-1;], indices)
+    indices=map(x->(x.-1).%(N-1).+1, indices)
+
+    #Create Points
+    for i in 1:length(indices)
+        push!(z, u[indices[i]])
+    end
 
     #Cranley-Patterson Rotation
     ucp=rand(d)
